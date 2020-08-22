a63e5f404a3f98b4dd3762df199629014fe152fb
Jeff Lyng <jely94@icloud.com>
2020-08-22 17:07:29 -0500

    added changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -24,7 +24,7 @@ with open(budget_data) as csvfile:
     #Reading the header row
     csv_header = next(csvreader)
 
-    #Reading the first row and track changes
+    #Reading the first row and track changes and count month
     first_row = next(csvreader)
     months = months + 1
     profitloss_total = profitloss_total + int(first_row[1])
@@ -32,10 +32,10 @@ with open(budget_data) as csvfile:
     
     #Read each row of data after the header & first row 
     for row in csvreader:
-        #Keeping track of the dates
+        #Keeping track of the dates by adding to list
         dates.append(row[0])
         
-        #Calculate the change and add it to change
+        #Calculate the change and add it to profits list and set new value.
         change = int(row[1])-value
         profits.append(change)
         value = int(row[1])
@@ -46,12 +46,12 @@ with open(budget_data) as csvfile:
         #Calculate the total amount of profit and losses
         profitloss_total = profitloss_total + int(row[1])
 
-    #Find the greatest increase in profits list and the date from the dates list
+    #Find the greatest increase in profits list and the coresponding date from the dates list
     greatest_increase = max(profits)
     greatest_index = profits.index(greatest_increase)
     greatest_date = dates[greatest_index]
 
-    #Find the greatest decrease in profits list and the date from the dates list
+    #Find the greatest decrease in profits list and the coresponding date from the dates list
     greatest_decrease = min(profits)
     worst_index = profits.index(greatest_decrease)
     worst_date = dates[worst_index]
@@ -69,14 +69,16 @@ print(f"Average Change: ${str(round(avg_change,2))}")
 print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
 print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
 
-#Output analysis to .txt file
-output_file = open("output.txt", "w")
+#Output analysis to .txt file and put in analysis folder
+output_path = os.path.join("Analysis", "Financial Analysis.txt")
+with open(output_path, 'w') as txtfile:
 
-line1 = "Financial Analysis"
-line2 = "---------------------"
-line3 = str(f"Total Months: {str(months)}")
-line4 = str(f"Total: ${str(profitloss_total)}")
-line5 = str(f"Average Change: ${str(round(avg_change,2))}")
-line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
-line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
-output_file.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
+    txtfile.write("Financial Analysis")
+    txtfile.write("---------------------")
+    txtfile.write(str(f"Total Months: {str(months)}"))
+    txtfile.write(str(f"Total: ${str(profitloss_total)}"))
+    txtfile.write(str(f"Average Change: ${str(round(avg_change,2))}"))
+    txtfile.write(str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})"))
+    txtfile.write(str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})"))
+
+
